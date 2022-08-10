74c3ef4269f93abcc3bcc48d9b1be397330717b0
Summer Mix <smix@tuta.io>
2022-08-10 20:41:37 +0300

    api path

---
+++ codin-game-ts/literal-object.ts
@@ -0,0 +1,28 @@
+class User {
+
+    _: string = '/user'
+
+    constructor(parent: string) {
+        this._ = parent + this._
+    }
+
+    get signup() { return this._ + '/signup' }
+}
+
+class Api {
+    user = new User(this._)
+
+    constructor(private _ = '/api') {
+    }
+}
+
+const api = new Api()
+
+console.debug(api.user.signup)
+
+const start = new Date().getTime()
+for (let i = 0; i < 10000000; i++) {
+    const path = api.user.signup
+}
+const ended = new Date().getTime()
+console.debug('time: ', ended - start)
